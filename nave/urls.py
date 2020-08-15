f27aa5403479becb558ab77fecf964b90bae0b89
thiagodiasv <thiago76ers@gmail.com>
2020-08-15 20:42:41 -0300

    feat: swagger of openapi

--- nave/urls.py
+++ nave/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from nave.api import urls as api_urls
@@ -13,4 +16,30 @@ urlpatterns = [
 
 urlpatterns += [
     path("api-auth/", include("rest_framework.urls")),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="nave API", default_version="v1", description="Test description",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    re_path(
+        r"openapi/swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "openapi/swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "openapi/redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
